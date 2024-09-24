e507917d3b60b145b60cb7f5a3104f26d027c42e
Shiz <141632242+Shazil0204@users.noreply.github.com>
2024-09-24 13:54:44 +0200

    recently started learning react native
    https://www.youtube.com/watch?v=ZBCUegTZF7M&t=1714s
    
    all Credits goes to this youtuber.

---
+++ LearningReactNative01/app/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from "react-native";
+import { COLORS, icons, images, SIZES } from "../constants";
+import { Stack, useRouter } from "expo-router";
+import { useState } from "react";
+
+const index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({});
